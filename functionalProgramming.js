0ee803f4e4c32936bcc140fb375d275d4c33a680
pam06 <impam269@gmail.com>
2022-06-23 20:09:51 +0530

    Functional Programming

---
+++ functionalProgramming.js
@@ -0,0 +1,23 @@
+//map()
+
+const numbers = [1, 5, 10, 15];
+// The associated function multiply each array number by 2
+const doubles = numbers.map(x => x * 2);
+
+console.log(numbers); // [1, 5, 10, 15] (no change)
+console.log(doubles); // [2, 10, 20, 30]
+
+
+//filter()
+const bigOnes = numbers.filter(x => x >= 10);
+
+console.log(numbers); // [1, 5, 10, 15] (no change)
+console.log(bigOnes); // [10, 15]
+
+
+//reduce()
+// Compute the sum of array elements
+const sum = numbers.reduce((acc, value) => acc + value, 0);
+
+console.log(numbers); // [1, 5, 10, 15] (no change)
+console.log(sum);     // 31
